0941d82f683698a14eac9309e2a8b3ae8f1bb047
hans <huangjianwu@chinasofti.com>
2019-07-24 17:20:59 +0800

    Add constants Demo

---
+++ HHSwiftTemplate/Constants/DeepLinkConstants.swift
@@ -0,0 +1,18 @@
+//
+//  DeepLinkConstants.swift
+//  HHSwiftTemplate
+//
+//  Created by chinaSoft on 2019/7/24.
+//  Copyright © 2019 wangcenttest. All rights reserved.
+//
+
+import Foundation
+
+struct DeepLinkConstants {
+    struct AModule {
+        static let home = "home"
+    }
+    struct BModule {
+        static let home = "home"
+    }
+}
