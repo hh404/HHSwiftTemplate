0dd57a98fe5e55deb99e5bd8d8a44c8c57962dd5
lucent <wangxiong001@chinasofti.com>
2019-07-24 16:02:55 +0800

    Initial Commit

---
+++ HHSwiftTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HHSwiftTemplate
+//
+//  Created by chinaSoft on 2019/7/24.
+//  Copyright © 2019 wangcenttest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
